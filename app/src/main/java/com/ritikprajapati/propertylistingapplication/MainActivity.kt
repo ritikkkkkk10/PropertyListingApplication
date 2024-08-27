565b2529d1a7d9986bcd68a8631890054c86d6a5
Ritik Prajapati <ritikreigns001@gmail.com>
2024-08-27 16:12:00 +0530

    toolbar option added

--- app/src/main/java/com/ritikprajapati/propertylistingapplication/MainActivity.kt
+++ app/src/main/java/com/ritikprajapati/propertylistingapplication/MainActivity.kt
@@ -4,11 +4,17 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.ritikprajapati.propertylistingapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var signout: Button
     private var auth: FirebaseAuth? = null
     @SuppressLint("MissingInflatedId")
@@ -23,6 +29,38 @@ class MainActivity : AppCompatActivity() {
             auth?.signOut() // Sign out the user
             navigateToLogin() // Navigate to the login screen
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val menuClickHandled = when (item.itemId) {
+            R.id.logout -> {
+                AlertDialog.Builder(this)
+                    .setTitle("Warning!")
+                    .setMessage("Confirm logout")
+                    .setPositiveButton("Yes") { _, _ ->
+                        FirebaseAuth.getInstance().signOut();
+                        val view = this@MainActivity.currentFocus ?: findViewById(android.R.id.content)
+                        Snackbar.make(view, "Logged out", Snackbar.LENGTH_SHORT).show()
+                        val intent = Intent(this, startActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    .setNegativeButton("No") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+        return menuClickHandled
     }
 
     private fun navigateToLogin() {
